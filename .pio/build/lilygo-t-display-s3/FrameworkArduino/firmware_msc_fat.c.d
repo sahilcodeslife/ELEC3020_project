925bec7fbe15f8996cbd133637feba69ddd16b57
sahilcodeslife <120233977+sahilcodeslife@users.noreply.github.com>
2025-09-17 20:41:55 +0800

    Delete .pio directory

--- .pio/build/lilygo-t-display-s3/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/lilygo-t-display-s3/FrameworkArduino/firmware_msc_fat.c.o: \
- C:/Users/sayno/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- C:/Users/sayno/OneDrive/Documents/GitHub/ELEC3020_project/.pio/libdeps/lilygo-t-display-s3/TFT_eSPI/User_Setups/Setup206_LilyGo_T_Display_S3.h \
- C:/Users/sayno/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
